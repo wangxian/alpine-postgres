31f4fbe2470cd6f1da11d85090f383a5c1d4e55c
wangxian <xian366@126.com>
2024-04-28 21:59:13 +0800

    UPDATE: startup.sh and README

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # alpine-postgres
 a docker image base on alpine with PostgreSQL
+
+## build local image (docker)
+
+```
+docker build -t wangxian/alpine-postgres:latest .
+```
+
+## build image (docker-compose)
+
+```
+cp .env-dist .env
+vim build .env # change environment if you need
+docker compose build
+```
+
+## Usage (docker)
+
+```
+# only superuser
+docker run -it --name postgres -p 5432:5432 -v ~/appdata/postgres:/app/postgres -e POSTGRES_DATABASE=admin -e POSTGRES_SUPER_PASSWORD=s6321..8 wangxian/alpine-postgres
+
+# use normal user
+docker run -it --name postgres -p 5432:5432 -v ~/appdata/postgres:/app/postgres -e POSTGRES_DATABASE=admin -e POSTGRES_USER=user -e POSTGRES_PASSWORD=user123..8 -e POSTGRES_SUPER_PASSWORD=s6321..8 wangxian/alpine-postgres
+```
+
+
+It will:
+- set password for 'postgres' (default is 's6321..8');
+- create a new db if set POSTGRES_DATABASE;
+- create an user and set his password
--- startup.sh
+++ startup.sh
@@ -6,34 +6,46 @@ if [ -z "$(ls -A "$PGDATA")" ]; then
     sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
 
     : ${POSTGRES_USER:="postgres"}
-    : ${POSTGRES_DB:=$POSTGRES_USER}
 
+    # 如果环境变量 POSTGRES_DATABASE 没有被设置（即没有值），则将其设置为 POSTGRES_USER 的值。
+    # 如果 POSTGRES_USER 也没有被设置，那么 POSTGRES_DATABASE 将被设置为空字符串。
+    : ${POSTGRES_DATABASE:=$POSTGRES_USER}
+
+    authMethod=md5
     if [ "$POSTGRES_PASSWORD" ]; then
       pass="PASSWORD '$POSTGRES_PASSWORD'"
-      authMethod=md5
-    else
-      echo "==============================="
-      echo "!!! NO PASSWORD SET !!! (Use \$POSTGRES_PASSWORD env var)"
-      echo "==============================="
-      pass=
-      authMethod=trust
     fi
     echo
 
+    # default super passwrod
+    if [ "$POSTGRES_SUPER_PASSWORD" = "" ]; then
+      POSTGRES_SUPER_PASSWORD=s6321..8
+      echo "...................... [i] default POSTGRES_SUPER_PASSWORD Password: $POSTGRES_SUPER_PASSWORD"
+    fi
+    echo
 
-    if [ "$POSTGRES_DB" != 'postgres' ]; then
-      createSql="CREATE DATABASE $POSTGRES_DB;"
+    # normal database
+    if [ "$POSTGRES_DATABASE" != 'postgres' ]; then
+      createSql="CREATE DATABASE $POSTGRES_DATABASE;"
       echo $createSql | su-exec postgres postgres --single -jE
       echo
     fi
 
+    # normal user
     if [ "$POSTGRES_USER" != 'postgres' ]; then
-      op=CREATE
-    else
-      op=ALTER
+      userSql="CREATE USER $POSTGRES_USER WITH PASSWORD $pass;"
+      echo $userSql | su-exec postgres postgres --single -jE
+      echo
+
+      if [ "$POSTGRES_DATABASE" != 'postgres' ]; then
+        createSql="GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DATABASE TO $POSTGRES_USER;"
+        echo $createSql | su-exec postgres postgres --single -jE
+        echo
+      fi
     fi
 
-    userSql="$op USER $POSTGRES_USER WITH SUPERUSER $pass;"
+    # super user pwd
+    userSql="ALTER USER postgres WITH SUPERUSER $POSTGRES_SUPER_PASSWORD;"
     echo $userSql | su-exec postgres postgres --single -jE
     echo
 
@@ -45,7 +57,7 @@ if [ -z "$(ls -A "$PGDATA")" ]; then
     # for f in /docker-entrypoint-initdb.d/*; do
     #     case "$f" in
     #         *.sh)  echo "$0: running $f"; . "$f" ;;
-    #         *.sql) echo "$0: running $f"; psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < "$f" && echo ;;
+    #         *.sql) echo "$0: running $f"; psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DATABASE" < "$f" && echo ;;
     #         *)     echo "$0: ignoring $f" ;;
     #     esac
     #     echo
